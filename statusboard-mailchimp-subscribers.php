98a332b10eb13632ae38c23d102aa1ba30647179
Thomas Gimesi <thomasgimesi@Thomass-Mac-mini.local>
2013-09-11 21:29:05 +0200

    Oops, that line was one too many.
    Deleted echo function, it was already in the html.

--- statusboard-mailchimp-subscribers.php
+++ statusboard-mailchimp-subscribers.php
@@ -57,7 +57,6 @@ $MailChimp = new MailChimp('YOURAPIKEY'); // enter your API-Key here
 $member_count = $MailChimp->call('lists/list');
 ?>
 
-<?php echo $member_count['data'][0]['stats']['member_count']; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
